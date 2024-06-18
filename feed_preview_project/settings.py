6273d116bba3c0e9c2d7d69f1c8fdfdeea688ff6
adrien leveille <adrienleveille.contact@gmail.com>
2024-06-18 14:07:32 +0200

    Added feed app to settings.py

--- feed_preview_project/settings.py
+++ feed_preview_project/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Feed Preview apps
+    'feed',
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
