05da0f07a29478e41b503d4b26582ec828b44e30
adrien leveille <adrienleveille.contact@gmail.com>
2024-06-18 14:26:09 +0200

    Install pillow, modifying requirements.txt file and database migrated.

--- feed/models.py
+++ feed/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Image(models.Model):
-    image = models.models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
